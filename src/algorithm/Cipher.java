8adbdd388e67477ae446d2561c35e10463298799
Dinika Senarath <dinika.15@cse.mrt.ac.lk>
2018-02-26 22:41:33 +0530

    Add files via upload
    First Commit

---
+++ src/algorithm/Cipher.java
@@ -0,0 +1,128 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Cipher {
+	
+	private ArrayList<Character> alphabet;
+	
+	public Cipher(){
+		this.alphabet=this.createAlphabet();
+	}
+	
+	public String encryptText(String plainText, String key){
+		String filledText = addFilling(plainText,key);
+		ArrayList<String> blocked = makeBlocks(filledText,key);
+		ArrayList<String> shuffledBlocks = transposeBlocks(blocked);
+		String encryptedText = transposeChars(shuffledBlocks,key);
+		System.out.println("Plain Text : "+plainText);
+		System.out.println("Encrypted Text : "+encryptedText);
+		return encryptedText;
+	}
+	
+	public String decryptText(String encryptedText, String key){
+		ArrayList<String> deBlocks = new ArrayList<String>();
+		deBlocks = makeBlocks(encryptedText,key);
+		String decryptedText = retransposeChars(retransposeBlocks(deBlocks),key);
+		System.out.println("Decrypted Text : "+decryptedText);
+		return decryptedText;
+	}
+	
+	private ArrayList<Character> createAlphabet(){
+		ArrayList<Character> alph = new ArrayList<Character>();
+		
+		for (int i=32;i<127;i++){
+			if(i!=92){
+				alph.add((char)i);
+			}
+		}
+		System.out.println(alph.toString());
+		return alph;
+	}
+	
+	private String addFilling(String plainText, String key){
+		int originalTextLength = plainText.length();
+		for (int j=0;j<(key.length()-(originalTextLength%key.length()));j++){
+			plainText += '#';
+		}
+		System.out.println("After padding added : "+plainText);
+		return plainText;
+	} 
+	
+	private ArrayList<String> makeBlocks(String plainText, String key){
+		ArrayList<String> blocks = new ArrayList<String>();
+		for (int k = 0; k < plainText.length(); k += key.length()) {
+			blocks.add((plainText.substring(k, k+key.length())));
+		}
+		System.out.println("After braking to blocks : "+blocks.toString());
+		return blocks;
+	} 
+	
+	private ArrayList<String> transposeBlocks(ArrayList<String> blocks){
+		ArrayList <String> shuffledBlocks = new ArrayList <String>();
+		for (int evenBlock=0; evenBlock<blocks.size(); evenBlock += 2){
+			shuffledBlocks.add((String) blocks.get(evenBlock));
+		}
+		for (int oddBlock=1; oddBlock<blocks.size(); oddBlock += 2){
+			shuffledBlocks.add((String) blocks.get(oddBlock));
+		}
+		System.out.println("After shifting blocks : "+shuffledBlocks);
+		return shuffledBlocks;
+	}
+	
+	private ArrayList<String> retransposeBlocks(ArrayList<String> blocks){
+		ArrayList <String> arrangedBlocks = new ArrayList <String>();
+		List<String> part1 = new ArrayList <String>();
+		List<String> part2 = new ArrayList <String>();
+		if(blocks.size()%2==0){
+			part1 = blocks.subList(0,(blocks.size()/2));
+			part2 = blocks.subList((blocks.size()/2), blocks.size());
+			for (int x=0;x<part1.size();x++){
+				arrangedBlocks.add(part1.get(x));
+				arrangedBlocks.add(part2.get(x));
+			}
+		}else{
+			part1 = blocks.subList(0,(blocks.size()/2)+1);
+			part2 = blocks.subList((blocks.size()/2)+1, blocks.size());
+			for (int x=0;x<part2.size();x++){
+				arrangedBlocks.add(part1.get(x));
+				arrangedBlocks.add(part2.get(x));
+			}arrangedBlocks.add(part1.get(part1.size()-1));
+		}
+		System.out.println("After rearranging blocks : "+arrangedBlocks);
+		return arrangedBlocks;
+	}
+	
+	private String transposeChars(ArrayList<String> shuffledBlocks,String key){
+		String encryptedText = "";
+		for (Iterator<String> i = shuffledBlocks.iterator(); i.hasNext();) {
+		    String block = i.next();
+		    for (int s=0;s<key.length();s++){
+		    	char textChar = block.charAt(s);
+		    	char keyChar = key.charAt(s);
+		    	int newIndex = (this.alphabet.indexOf(textChar)+this.alphabet.indexOf(keyChar))%94;
+		    	encryptedText += this.alphabet.get(newIndex);
+		    }
+		}
+		System.out.println("After transposing chars : "+encryptedText);
+		return encryptedText;
+	}
+	
+	private String retransposeChars(ArrayList<String> blocks,String key){
+		String decryptedText = "";
+		for (Iterator<String> i = blocks.iterator(); i.hasNext();) {
+		    String block = i.next();
+		    for (int d=0;d<key.length();d++){
+		    	char cryptChar = block.charAt(d);
+		    	char keyChar = key.charAt(d);
+		    	int newIndex = (this.alphabet.indexOf(cryptChar)+94-this.alphabet.indexOf(keyChar))%94;
+		    	decryptedText += this.alphabet.get(newIndex);
+		    }
+		}
+		System.out.println("After transposing chars : "+decryptedText);
+		return decryptedText;
+	}
+
+}
---
+++ src/userInterface/MainWindow.java
@@ -0,0 +1,257 @@
+package userInterface;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import algorithm.Cipher;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MainWindow {
+
+	private JFrame frame;
+	private JTextField keyField;
+	private String plainText;
+	private String key;
+	private String enryptedText;
+	private File filePath;
+	private String encryptedTextRead;
+	private String decryptedText;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		final JLabel pathLabel = new JLabel();
+		pathLabel.setText("(Select a text file)");
+		pathLabel.setBounds(110, 67, 314, 49);
+		frame.getContentPane().add(pathLabel);
+		
+		JLabel lblProductCipher = new JLabel("Product Cipher ");
+		lblProductCipher.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblProductCipher.setBounds(147, 28, 121, 28);
+		frame.getContentPane().add(lblProductCipher);
+		
+		JButton btnNewButton = new JButton("Choose File");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser fc = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+		        fc.setFileFilter(filter);
+				int ss = fc.showOpenDialog(null);
+				if(ss == JFileChooser.APPROVE_OPTION){
+					System.out.println(fc.getSelectedFile().getAbsolutePath());
+					pathLabel.setText(fc.getSelectedFile().getAbsolutePath());
+					filePath = fc.getSelectedFile();
+					
+				}
+			}
+		});
+		btnNewButton.setBounds(110, 117, 111, 28);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("Encypt");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (keyField.getText().length()<6){
+					JOptionPane.showConfirmDialog(null,"key should have a length of at least 6 characters", 
+							"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+				}else{
+					plainText = "";
+					String line=null;
+					try {
+						FileReader fReader = new FileReader(filePath);
+						BufferedReader bufferedReader =new BufferedReader(fReader);
+		                while((line = bufferedReader.readLine()) != null) {
+		                    plainText+=line;
+		                }   
+		                bufferedReader.close();
+					} catch (FileNotFoundException e) {
+						JOptionPane.showConfirmDialog(null,"File not found", 
+								"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+						System.out.println("File not found");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						System.out.println("Failed to read file");
+						JOptionPane.showConfirmDialog(null,"Failed to read file", 
+								"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+					}
+					
+					Cipher cipher = new Cipher();
+					try{
+						enryptedText = cipher.encryptText(plainText, keyField.getText());
+						try {
+							String name = filePath.getName();
+							String directory= filePath.getParent();
+							String newFile = "out.txt";
+							int pos = name.lastIndexOf(".");
+							if (pos > 0) {
+							    newFile = directory+"\\"+name.substring(0, pos)+"_encrypted.txt";
+							}
+				            FileWriter fileWriter =new FileWriter(newFile);
+				            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				            bufferedWriter.write(enryptedText);
+				            bufferedWriter.close();
+				            JOptionPane.showMessageDialog(null, "Encrypted file saved successfully");
+				            keyField.setText("");
+				    		pathLabel.setText("(Select a text file)");
+				        }
+				        catch(IOException ex) {
+				        	JOptionPane.showConfirmDialog(null,"Error writing to file", 
+									"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+				            System.out.println("Error writing to file");
+				        }
+						
+					} catch (Exception e) {
+						JOptionPane.showConfirmDialog(null,"Failed to encrypt file", 
+								"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+						System.out.println("Failed to encrypt file");
+					}
+				}
+		}});
+		btnNewButton_2.setBounds(100, 211, 89, 23);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Decrypt");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				encryptedTextRead = "";
+				String line=null;
+				try {
+					FileReader fReader = new FileReader(filePath);
+					BufferedReader bufferedReader =new BufferedReader(fReader);
+	                while((line = bufferedReader.readLine()) != null) {
+	                	encryptedTextRead+=line;
+	                }   
+	                bufferedReader.close();
+				} catch (FileNotFoundException e) {
+					JOptionPane.showConfirmDialog(null,"File not found", 
+							"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE); 
+					System.out.println("File not found");
+				} catch (IOException e) {
+					System.out.println("Failed to read file");
+					JOptionPane.showConfirmDialog(null,"Error reading file", 
+							"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+				}
+				
+				Cipher cipher = new Cipher();
+				try{
+					decryptedText = cipher.decryptText(encryptedTextRead, keyField.getText());
+					try {
+						String name = filePath.getName();
+						String directory= filePath.getParent();
+						String newFile = "out.txt";
+						int pos = name.lastIndexOf(".");
+						if (pos > 0) {
+						    newFile = directory+"\\"+name.substring(0, pos)+"_decrypted.txt";
+						}
+			            FileWriter fileWriter =new FileWriter(newFile);
+			            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			            bufferedWriter.write(decryptedText);
+			            bufferedWriter.close();
+			            JOptionPane.showMessageDialog(null, "Decrypted file saved successfully");
+			            keyField.setText("");
+			            pathLabel.setText("(Select a text file)");
+			        }
+			        catch(IOException ex) {
+			        	JOptionPane.showConfirmDialog(null,"Error writing to file", 
+								"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+			            System.out.println("Error writing to file");
+			        }
+					
+				} 
+				catch (Exception e) {
+					JOptionPane.showConfirmDialog(null,"Failed to decrypt file", 
+							"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+					System.out.println("Failed to decrypt file");
+				}
+			}
+		});
+		btnNewButton_3.setBounds(251, 211, 89, 23);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		
+		keyField = new JTextField();
+		keyField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent ev) {
+				char ch = ev.getKeyChar();
+				if(ch == ev.VK_SPACE || ch == ev.VK_BACK_SLASH){
+					JOptionPane.showConfirmDialog(null,"Key cannot contain white spaces or back slashes(\\)", 
+							"Error",JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		keyField.setBounds(110, 164, 269, 20);
+		frame.getContentPane().add(keyField);
+		keyField.setColumns(10);
+		
+		JLabel lblTextFile = new JLabel("Text File");
+		lblTextFile.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblTextFile.setBackground(Color.WHITE);
+		lblTextFile.setBounds(20, 81, 72, 20);
+		frame.getContentPane().add(lblTextFile);
+		
+		JLabel lblSecretKey = new JLabel("Secret Key");
+		lblSecretKey.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblSecretKey.setBackground(Color.WHITE);
+		lblSecretKey.setBounds(20, 166, 76, 14);
+		frame.getContentPane().add(lblSecretKey);
+		
+		
+	}
+}
